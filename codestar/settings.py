3152c68ea20dd6c47c1a397013c48340a269ecc0
Dickson Olejeme <olejemedickson@gmail.com>
2024-01-20 12:50:54 +0000

    Add secret key

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,10 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--p_sl8g9tl_&8dg94xm&p9hnnn!1qmp+4_zuhx%nc6yg5%)6p^'
+# SECRET_KEY = 'django-insecure--p_sl8g9tl_&8dg94xm&p9hnnn!1qmp+4_zuhx%nc6yg5%)6p^'
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.0', '.herokuapp.com']
 
