154e799c6a7ea380f4157827938dd3f09816f909
Dickson Olejeme <olejemedickson@gmail.com>
2024-01-25 08:58:25 +0000

    Add comment model

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 STATUS = ((0, "draft"), (1, "published"))
+APPROVED = ((0, "True"), (1, "False"))
 
 # Create your models here.
 class Post(models.Model):
@@ -14,3 +15,14 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+    
+    
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+        
