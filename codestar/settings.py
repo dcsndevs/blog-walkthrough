e12fa8dd11776d1f8988aff2828940e406a71a8d
Dickson <olejemedickson@gmail.com>
2024-02-01 09:40:21 +0000

    Customise Allauth pages

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com', '*-dcsndevs-blogwalkthroug-ungq84xr8x9.ws-us107.gitpod.io']
 
